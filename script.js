b772002cfa80db064d6d9cd51ed9d78be536d7d9
Yuliia Skaskevych <skaskevich2000@gmail.com>
2023-03-23 05:26:48 +0100

    hw_23

---
+++ script.js
@@ -0,0 +1,18 @@
+function createMatrix() {
+  let tbl = document.createElement("table");
+  let tblBody = document.createElement("tbody");
+  for (let y = 0; y < 10; y++) {
+    let row = document.createElement("tr");
+    for (let x = 0; x < 10; x++) {
+      let columns = document.createElement("td");
+      let columnText = document.createTextNode(x + 10 * y + 1);
+      columns.appendChild(columnText);
+      row.appendChild(columns);
+    }
+    tblBody.appendChild(row);
+  }
+  tbl.appendChild(tblBody);
+  document.body.appendChild(tbl);
+  tbl.setAttribute("border", "1");
+}
+createMatrix();
